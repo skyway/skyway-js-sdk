d14e065b08502e2f14dd449665438225aa88ec77
Alan Iida <alan.m.shelly@gmail.com>
2016-02-10 16:27:14 +0900

    add tests for initializeServerConnection

--- tests/test-peer.js
+++ tests/test-peer.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const Peer      = require('../src/peer');
+const Socket    = require('../src/socket');
 const assert    = require('power-assert');
 const util      = require('../src/util');
+const sinon     = require('sinon');
 
 describe.only('Peer', () => {
   describe('Constructor', () => {
     const apiKey = 'abcdefgh-1234-5678-jklm-zxcvasdfqwrt';
+
     it('should create a Peer object', () => {
       const peer = new Peer({
         key: apiKey
@@ -14,6 +17,7 @@ describe.only('Peer', () => {
       assert(peer);
       assert(peer instanceof Peer);
     });
+
     it('should create a Peer object with default options', () => {
       const peer = new Peer({
         key: apiKey
@@ -26,6 +30,7 @@ describe.only('Peer', () => {
       assert(peer.options.config === util.defaultConfig);
       assert(peer.options.turn === true);
     });
+
     it('should create a Peer object with options overwritten', () => {
       const config = {iceServers: []};
       const peer = new Peer({
@@ -45,6 +50,7 @@ describe.only('Peer', () => {
       assert(typeof peer.options.token === 'string');
       assert(peer.options.turn === true);
     });
+
     // TODO: Implement after initializeServerConnection
     // it('should create a Peer object with ID', () => {
     //   const peer = new Peer('myID', {
@@ -53,6 +59,7 @@ describe.only('Peer', () => {
     //   assert(peer);
     //   assert(peer instanceof Peer);
     // });
+
     it('should not create a Peer object with invalid ID', done => {
       let peer;
       try {
@@ -64,6 +71,7 @@ describe.only('Peer', () => {
         done();
       }
     });
+
     it('should not create a Peer object with invalid API key', done => {
       let peer;
       try {
@@ -74,6 +82,71 @@ describe.only('Peer', () => {
         assert(peer === undefined);
         done();
       }
+    });
+
+    it('should contain a Socket object', () => {
+      const peer = new Peer({
+        key: apiKey
+      });
+
+      assert(peer.socket);
+      assert(peer.socket instanceof Socket);
+    });
+
+    it('should call _handleMessage on a socket "message" event', () => {
+      const peer = new Peer({
+        key: apiKey
+      });
+
+      const testMsg = 'test message';
+      const spy = sinon.spy(peer._handleMessage);
+
+      peer.socket.emit('message', testMsg);
+      assert(spy.calledOnce === true);
+      assert(spy.calledWith(testMsg) === true);
+    });
+
+    it('should abort on a socket "error"', done => {
+      const peer = new Peer({
+        key: apiKey
+      });
+
+      const errMsg = 'test error';
+
+      peer.on('error', err => {
+        assert(err.type === 'socket-error');
+        assert(err.message === errMsg);
+        done();
+      });
+
+      peer.socket.emit('error', errMsg);
+    });
+
+    it('should abort and disconnect on a socket "disconnect" event', done => {
+      const peer = new Peer({
+        key: apiKey
+      });
+
+      const errMsg = 'test error';
+
+      peer.on('error', err => {
+        assert(err.type === 'socket-error');
+        assert(err.message === errMsg);
+
+        assert(peer._disconnectCalled === true);
+        done();
+      });
+
+      peer.socket.emit('disconnect', 'test error');
+    });
+
+    it('should call destroy onbeforeunload', () => {
+      const peer = new Peer({
+        key: apiKey
+      });
+
+      window.onbeforeunload();
+      assert(peer._destroyCalled === true);
     });
   });
 });
