127425a62fd485d980418d4b441f4b0b48379601
Ewan Leaver <ewanleaver@gmail.com>
2016-02-17 10:14:55 +0900

    Refactored stubbing code into beforeEach() funciton

--- tests/test-mediaConnection.js
+++ tests/test-mediaConnection.js
@@ -5,25 +5,38 @@ const assert     = require('assert');
 const proxyquire = require('proxyquire');
 const sinon      = require('sinon');
 
+let Connection;
+let MediaConnection;
+
 describe('MediaConnection', () => {
+
+  let stub;
+  let spy;
+
+  beforeEach(() => {
+    stub = sinon.stub();
+    spy = sinon.spy();
+
+    stub.returns({
+      startConnection: spy
+    });
+
+    Connection = proxyquire(
+      '../src/connection',
+      {'./negotiator': stub}
+    );
+    MediaConnection = proxyquire(
+      '../src/mediaConnection',
+      {'./connection': Connection}
+    );
+  });
+
+  afterEach(() => {
+    //stub.restore();
+  });
+
   describe('Constructor', () => {
     it('should call negotiator\'s startConnection method when created', () => {
-      const stub = sinon.stub();
-      const spy = sinon.spy();
-
-      stub.returns({
-        startConnection: spy
-      });
-
-      const Connection = proxyquire(
-        '../src/connection',
-        {'./negotiator': stub}
-      );
-      const MediaConnection = proxyquire(
-        '../src/mediaConnection',
-        {'./connection': Connection}
-      );
-
       const peerId = 'peerId';
       const peer = new Peer(peerId, {});
 
@@ -32,5 +45,9 @@ describe('MediaConnection', () => {
       assert(mc);
       assert(spy.calledOnce);
     });
+  });
+
+  it('should set and emit the remote stream upon receiving it', () => {
+    
   });
 });
