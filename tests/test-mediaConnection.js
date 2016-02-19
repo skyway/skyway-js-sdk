0507458e1b4f00321c297e2e7d8c67c9c27722ef
Ewan Leaver <ewanleaver@gmail.com>
2016-02-19 17:04:33 +0900

    Fix test broken after merge from dev

--- tests/test-mediaConnection.js
+++ tests/test-mediaConnection.js
@@ -44,6 +44,14 @@ describe('MediaConnection', () => {
       assert(mc);
       assert(startSpy.calledOnce);
     });
+
+    it('should store any messages passed in when created', () => {
+      const peerId = 'peerId';
+      const peer = new Peer(peerId, {});
+      const mc = new MediaConnection(peer, {_stream: {}, _queuedMessages: ['message']});
+
+      assert.deepEqual(mc.options._queuedMessages, ['message']);
+    });
   });
 
   describe('Add Stream', () => {
@@ -78,24 +86,6 @@ describe('MediaConnection', () => {
       assert(spy.calledWith('stream', 'fakeStream') === true);
 
       spy.restore();
-    });
-
-    it('should store any messages passed in when created', () => {
-      const stub = sinon.stub();
-      const Connection = proxyquire(
-        '../src/connection',
-        {'./negotiator': stub}
-      );
-      const MediaConnection = proxyquire(
-        '../src/mediaConnection',
-        {'./connection': Connection}
-      );
-
-      const peerId = 'peerId';
-      const peer = new Peer(peerId, {});
-      const mc = new MediaConnection(peer, {_stream: {}, _queuedMessages: ['message']});
-
-      assert(mc.options._queuedMessages === ['message']);
     });
   });
 });
