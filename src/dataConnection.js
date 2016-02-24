bea5029d981b57a80af109caf967c9fe5ca5eef8
Ewan Leaver <ewanleaver@shogouki.local>
2016-02-24 11:27:00 +0900

    Implemented constructor to match peerJS

--- src/dataConnection.js
+++ src/dataConnection.js
@@ -3,13 +3,32 @@
 const Connection = require('./connection');
 
 class DataConnection extends Connection {
-  constructor(peer, provider, options) {
-    super(peer, provider, options);
+  constructor(peer, options) {
+    super(peer, options);
 
     this._idPrefix = 'dc_';
     this.type = 'data';
     this.label = this.options.label || this.id;
     this.serialization = this.options.serialization;
+
+    // Data channel buffering.
+    this._buffer = [];
+    this._buffering = false;
+    this.bufferSize = 0;
+
+    // For storing large data.
+    this._chunkedData = {};
+
+    if (this.options._payload) {
+      this._peerBrowser = this.options._payload.browser;
+    }
+
+    this._negotiator.startConnection(
+      this,
+      this.options._payload || {
+        originator: true
+      }
+    );
   }
 
   initialize(dc) {
