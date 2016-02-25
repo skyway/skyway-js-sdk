4ed1ad7349523f0c12a9d92be5b3eeefca9daafe
Saki Homma <sakkuru0323@gmail.com>
2016-02-25 13:58:47 +0900

    remove unused args

--- tests/test-negotiator.js
+++ tests/test-negotiator.js
@@ -9,14 +9,14 @@ const MediaConnection = require('../src/mediaConnection');
 describe('Negotiator', () => {
   describe('Constructor', () => {
     it('should be initialized with a mediaConnection', () => {
-      const mc = new MediaConnection({}, {});
+      const mc = new MediaConnection({});
       const negotiator = new Negotiator(mc);
 
       assert(negotiator._connection === mc);
     });
 
     it('should be initialized with a socket and a dataConnection', () => {
-      const dc = new DataConnection({}, {});
+      const dc = new DataConnection({});
       const negotiator = new Negotiator(dc);
 
       assert(negotiator._connection === dc);
