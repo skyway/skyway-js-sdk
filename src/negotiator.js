4176457f12d81c42caf1880e35c9c18f13ebe59a
Saki Homma <sakkuru0323@gmail.com>
2016-03-03 16:03:25 +0900

    add _setupPCListeners and _makeOffer

--- src/negotiator.js
+++ src/negotiator.js
@@ -1,15 +1,100 @@
 'use strict';
 
-// const util = require('./util');
+const util = require('./util');
 
-class Negotiator {
+const EventEmitter = require('events');
+
+class Negotiator extends EventEmitter {
   constructor() {
+    super();
+
     this._idPrefix = 'pc_';
   }
 
   startConnection(options) {
     // TODO: Remove lint bypass
     console.log(options);
+  }
+
+  _createPeerConnection(type) {
+    return {};
+  }
+
+  _setupPCListeners(pc) {
+
+    pc.onaddstream = evt => {
+      util.log('Received remote media stream');
+      var stream = evt.stream;
+      this.emit('addStream', stream);
+    };
+
+    pc.ondatachannel = evt => {
+      util.log('Received data channel');
+      var dc = evt.channel;
+      this.emit('dcReady', dc);
+    };
+
+    pc.onicecandidate = evt => {
+      const candidate = evt.candidate;
+      console.log(candidate)
+      if (!candidate) {
+        util.log('ICE canddidates gathering complete');
+      } else {
+        util.log('Generated ICE candidate for:', candidate);
+        this.emit('iceCandidate', candidate);
+      }
+    };
+
+    pc.oniceconnectionstatechange = () => {
+      switch (pc.iceConnectionState) {
+        case 'disconnected':
+        case 'failed':
+          util.log('iceConnectionState is disconnected, closing connection');
+          this.emit('iceConnectionDisconnected');
+          break;
+        case 'completed':
+          pc.onicecandidate = () => {};
+          break;
+      }
+    };
+
+    pc.onnegotiationneeded = () => {
+      util.log('`negotiationneeded` triggered');
+      this._makeOffer();
+    };
+
+    pc.onremovestream = () => {
+      util.log('`removestream` triggerd');
+    };
+
+    pc.onsignalingstatechange = evt => {
+      util.log('`signalingstatechange` triggerd');
+    };
+
+    return pc;
+  }
+
+  _makeOffer() {
+    var pc = this._pc;
+
+    if(!!pc.remoteDescription && !!pc.remoteDescription.type) return;
+
+    pc.createOffer(option)
+    .then(offer => {
+      util.log('Created offer.');
+      console.log(offer)
+      return pc.setLocalDescription(offer);
+    }, error => {
+      this.emitError('webrtc', err);
+      util.log('Failed to createOffer, ', err);
+    }).then(offer => {
+      console.log("set localDescription")
+      util.log('Set localDescription: offer');
+      this.emit('offerCreated', offer);
+    }, error => {
+      this.emitError('webrtc', err);
+      util.log('Failed to setLocalDescription, ', err);
+    });
   }
 
   cleanup() {
--- tests/test-negotiator.js
+++ tests/test-negotiator.js
@@ -3,6 +3,7 @@
 const assert     = require('power-assert');
 
 const Negotiator      = require('../src/negotiator');
+const sinon      = require('sinon');
 
 describe('Negotiator', () => {
   describe('Constructor', () => {
@@ -12,5 +13,127 @@ describe('Negotiator', () => {
       assert(negotiator);
       assert(negotiator instanceof Negotiator);
     });
+  });
+
+  describe('_setupPCListeners', () => {
+
+    it('should set up PeerConnection listeners', () => {
+      const negotiator = new Negotiator();
+      const pc = negotiator._createPeerConnection('media');
+
+      negotiator._setupPCListeners(pc);
+      assert(pc.onaddstream);
+      assert(pc.ondatachannel);
+      assert(pc.onicecandidate);
+      assert(pc.oniceconnectionstatechange);
+      assert(pc.onnegotiationneeded);
+      assert(pc.onremovestream);
+      assert(pc.onsignalingstatechange);
+    });
+
+    describe('RTCPeerConnection\'s event listeners', () => {
+
+      let negotiator;
+      let pc;
+      let ev;
+
+      beforeEach(() => {
+        negotiator = new Negotiator();
+        pc = negotiator._createPeerConnection('media');
+        negotiator._setupPCListeners(pc);
+        ev = {};
+      })
+
+      context('when pc listen \'addstream\'', () => {
+        it('should emit \'addStream\' with remote stream', done => {
+          ev.stream = 'stream';
+          negotiator.on('addStream', stream => {
+            assert(stream, ev.stream);
+            done();
+          });
+
+          pc.onaddstream(ev);
+        })
+      })
+      context('when pc listen \'datachannel\'', () => {
+        it('should emit \'dcReady\' with datachannel', done => {
+          ev.channel = 'dc';
+          negotiator.on('dcReady', dc => {
+            assert(dc, ev.channel);
+            done();
+          });
+          pc.ondatachannel(ev);
+        })
+      })
+      context('when pc listen \'icecandidate\'', () => {
+        it('should emit \'iceCandidate\' with ice candidate', done => {
+          ev.candidate = 'candidate';
+          negotiator.on('iceCandidate', candidate => {
+            assert(candidate, ev.candidate);
+            done();
+          });
+
+          pc.onicecandidate(ev);
+        })
+      })
+
+      context('when pc listen \'oniceconnectionstatechange\'', () => {
+        context('when pc.iceConnectionState is \'disconnected\'', () => {
+          it('should emit \'iceConnectionDisconnected\'', done => {
+            negotiator.on('iceConnectionDisconnected', () => {
+              done();
+            });
+            pc.iceConnectionState = 'disconnected';
+            pc.oniceconnectionstatechange();
+          })
+        })
+        context('when pc.iceConnectionState is \'failed\'', () => {
+          it('should emit \'iceConnectionDisconnected\'', done => {
+            negotiator.on('iceConnectionDisconnected', () => {
+              done();
+            });
+            pc.iceConnectionState = 'failed';
+            pc.oniceconnectionstatechange();
+          })
+        })
+        context('when pc.iceConnectionState is \'completed\'', () => {
+          it('should set pc.onicecandidate noop', () => {
+            pc.iceConnectionState = 'completed';
+            pc.oniceconnectionstatechange();
+            const noop = () => {};
+            console.log(pc.onicecandidate)
+            console.log(noop)
+            assert(pc.onicecandidate == () => {});
+          })
+        })
+      })
+
+      context('when pc listen \'negotiationneeded\'', () => {
+        it('should call pc.createOffer', () => {
+          console.log(pc)
+          const spy = sinon.spy(pc, 'createOffer');
+          pc.onnegotiationneeded();
+
+          assert(spy.calledOnce, true);
+        });
+        it('should call pc.setLocalDescription', () => {
+          const spy = sinon.spy(pc, 'setLocalDescription');
+          pc.onnegotiationneeded();
+
+          assert(spy.calledOnce, true);
+        })
+      })
+
+      describe('pc.onremovestream', () => {
+        it('do nothing', () => {
+        })
+      })
+
+      describe('pc.onsignalingstatechange', () => {
+        it('do nothing', () => {
+        })
+      })
+    })
+
   });
 });
