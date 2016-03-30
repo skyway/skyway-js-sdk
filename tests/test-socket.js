c81ab15835781de5063205f61d26e0fb2709480f
Ewan Leaver <ewanleaver@gmail.com>
2016-03-30 15:36:33 +0900

    Test for socket re-emitting an incoming ROOM_USER_LEFT event

--- tests/test-socket.js
+++ tests/test-socket.js
@@ -270,5 +270,31 @@ describe('Socket', () => {
         assert(spy.calledWith(util.MESSAGE_TYPES.ROOM_DATA.key, data));
       });
     });
+
+    describe('Leave', () => {
+      let socket;
+      let peerId = 'peerId';
+      let token = 'token';
+
+      beforeEach(() => {
+        let apiKey = 'apiKey';
+
+        socket = new Socket(false, 'localhost', serverPort, apiKey);
+      });
+
+      it('should emit a message to the Peer upon a ROOM_USER_LEFT acknowledgement', () => {
+        const data = {roomName: 'testRoom'};
+
+        socket.start(peerId, token);
+
+        let spy = sinon.spy(socket, 'emit');
+        assert.equal(spy.callCount, 0);
+
+        socket._io._fakeMessage[util.MESSAGE_TYPES.ROOM_USER_LEFT.key](data);
+
+        assert(spy.calledWith(util.MESSAGE_TYPES.ROOM_USER_LEFT.key, data));
+        assert.equal(spy.callCount, 1);
+      });
+    });
   });
 });
