b42949dbd34226f630cf62730a59ef617f809bac
Ewan Leaver <ewanleaver@gmail.com>
2016-04-19 15:37:36 +0900

    Basic RoomConnection Implementation

---
+++ src/roomConnection.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Connection = require('./connection');
+const Negotiator = require('./negotiator');
+const util = require('./util');
+
+class RoomConnection extends Connection {
+  constructor(remoteId, options) {
+    super(remoteId, options);
+
+    this._idPrefix = 'rc_';
+    this.type = 'room';
+
+    this.localStream = this.options._stream;
+    this._pcAvailable = false;
+
+    if (this.localStream) {
+      this._negotiator.startConnection(
+        {
+          type:       'room',
+          _stream:    this.localStream,
+          originator: true
+        },
+        this.options.pcConfig
+      );
+      this._pcAvailable = true;
+    }
+
+    // There should be no 'answer' method or queued events for a RoomConnection
+    // (Every user joins of their own accord)
+
+    this._negotiator.on(Negotiator.EVENTS.addStream.key, remoteStream => {
+      util.log('Receiving stream', remoteStream);
+
+      this.remoteStream = remoteStream;
+      this.emit('stream', remoteStream);
+    });
+  }
+}
+
+module.exports = RoomConnection;
