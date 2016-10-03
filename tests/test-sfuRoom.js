4d20c1f160c768ba4df86969f0d1733a45c53429
Alan Iida <alan.m.shelly@gmail.com>
2016-10-03 15:04:49 +0900

    add tests for sfuRoom.replaceStream

--- tests/test-sfuRoom.js
+++ tests/test-sfuRoom.js
@@ -217,6 +217,34 @@ describe('SFURoom', () => {
         });
       });
 
+      describe('negotiationNeeded', () => {
+        it('should emit an offerRequest message event', done => {
+          sfuRoom.on(SFURoom.MESSAGE_EVENTS.offerRequest.key, offerRequestMessage => {
+            assert.equal(offerRequestMessage.roomName, sfuRoomName);
+            done();
+          });
+
+          sfuRoom._negotiator.emit(Negotiator.EVENTS.negotiationNeeded.key);
+        });
+
+        it('should set up an answerCreated event handler which emits an answer event once', done => {
+          const answer = {};
+
+          sfuRoom._negotiator.emit(Negotiator.EVENTS.negotiationNeeded.key);
+
+          // will fail if done() is called twice.
+          sfuRoom.on(SFURoom.MESSAGE_EVENTS.answer.key, answerMessage => {
+            assert.equal(answerMessage.roomName, sfuRoomName);
+            assert.equal(answerMessage.answer, answer);
+            done();
+          });
+
+          // call twice to make sure answerMessage is only emitted once.
+          sfuRoom._negotiator.emit(Negotiator.EVENTS.answerCreated.key, answer);
+          sfuRoom._negotiator.emit(Negotiator.EVENTS.answerCreated.key, answer);
+        });
+      });
+
       describe('iceCandidatesComplete', () => {
         it('should emit an answer message event', done => {
           const answer = {};
