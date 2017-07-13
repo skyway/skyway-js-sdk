83c09678d5488a684e0f075e184682785c8e75bf
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-07-13 10:33:41 +0900

    Add webkit

--- src/webrtcShim.js
+++ src/webrtcShim.js
@@ -1,5 +1,6 @@
 const RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
-const RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection;
+const RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection
+                                                   || window.mozRTCPeerConnection;
 const RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
 
 module.exports = {
