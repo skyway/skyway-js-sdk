759792b5300e41876f9a3cbf04d160a4408c65c9
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-06-25 18:49:53 +0900

    Fix comments [skip ci]

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 
-// base
 const config = {
   mode: 'development',
   entry: {
@@ -14,7 +13,7 @@ const config = {
   },
   module: {
     rules: [
-      // keep this for karma
+      // keep this place for karma
     ],
   },
   plugins: [
@@ -23,7 +22,7 @@ const config = {
   ],
 };
 
-// from `npm run build, exports both `.js`, `.min.js`
+// from `npm run build`, exports both `.js`, `.min.js`
 if (process.env.NODE_ENV === 'production') {
   const minConf = Object.assign({}, config, {
     mode: 'production',
@@ -31,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   module.exports = [config, minConf];
-  // from karma, exports only not minified
+  // from `npm run dev` and `npm t`(= karma-webpack), exports dev config
 } else {
   module.exports = config;
 }
