7e908f60adb1bfe7a20899ed9fa632f004270ece
matsub <matsub.rk@gmail.com>
2019-01-31 14:55:47 +0900

    Add new examples

---
+++ examples/_shared/key.js
@@ -0,0 +1,1 @@
+window.__SKYWAY_KEY__ = '<YOUR_KEY_HERE>';
---
+++ examples/p2p-data/script.js
@@ -0,0 +1,90 @@
+const Peer = window.Peer;
+
+(async function main() {
+  const localId = document.querySelector('[data-local-id]');
+  const localText = document.querySelector('[data-local-text]');
+  const connectTrigger = document.querySelector('[data-connect-trigger]');
+  const closeTrigger = document.querySelector('[data-close-trigger]');
+  const sendTrigger = document.querySelector('[data-send-trigger]');
+  const remoteId = document.querySelector('[data-remote-id]');
+  const messages = document.querySelector('[data-messages]');
+
+  const peer = new Peer({
+    key: window.__SKYWAY_KEY__,
+    debug: 3,
+  });
+
+  // Register connecter handler
+  connectTrigger.addEventListener('click', () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const dataConnection = peer.connect(remoteId.value);
+
+    dataConnection.once('open', async () => {
+      messages.textContent += `=== DataConnection has been opened ===\n`;
+
+      sendTrigger.addEventListener('click', onClickSend);
+    });
+
+    dataConnection.on('data', data => {
+      messages.textContent += `Remote: ${data}\n`;
+    });
+
+    dataConnection.once('close', () => {
+      messages.textContent += `=== DataConnection has been closed ===\n`;
+      sendTrigger.removeEventListener('click', onClickSend);
+    });
+
+    // Register closing handler
+    closeTrigger.addEventListener('click', () => dataConnection.close(), {
+      once: true,
+    });
+
+    function onClickSend() {
+      const data = localText.value;
+      dataConnection.send(data);
+
+      messages.textContent += `You: ${data}\n`;
+      localText.value = '';
+    }
+  });
+
+  peer.once('open', id => (localId.textContent = id));
+
+  // Register connectee handler
+  peer.on('connection', dataConnection => {
+    dataConnection.once('open', async () => {
+      messages.textContent += `=== DataConnection has been opened ===\n`;
+
+      sendTrigger.addEventListener('click', onClickSend);
+    });
+
+    dataConnection.on('data', data => {
+      messages.textContent += `Remote: ${data}\n`;
+    });
+
+    dataConnection.once('close', () => {
+      messages.textContent += `=== DataConnection has been closed ===\n`;
+      sendTrigger.removeEventListener('click', onClickSend);
+    });
+
+    // Register closing handler
+    closeTrigger.addEventListener('click', () => dataConnection.close(), {
+      once: true,
+    });
+
+    function onClickSend() {
+      const data = localText.value;
+      dataConnection.send(data);
+
+      messages.textContent += `You: ${data}\n`;
+      localText.value = '';
+    }
+  });
+
+  peer.on('error', console.error);
+})();
