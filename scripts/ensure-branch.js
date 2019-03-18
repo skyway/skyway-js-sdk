c75e5f704f7ca22d3bc0d6b0ba6b53fd900be411
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-03-18 13:30:10 +0900

    Check branch

--- scripts/ensure-branch.js
+++ scripts/ensure-branch.js
@@ -10,21 +10,29 @@ const { CIRCLE_BRANCH, CIRCLE_PULL_REQUEST, GITHUB_TOKEN } = process.env;
 
   // eg. https://github.com/skyway/skyway-js-sdk/pull/155
   const [, , , owner, repo, , number] = CIRCLE_PULL_REQUEST.split('/');
-  console.log(owner, repo, number);
 
   const octokit = new Octokit({ auth: GITHUB_TOKEN });
-  const result = await octokit.pulls.get({
+  const { data: { base: ref } } = await octokit.pulls.get({
     owner,
     repo,
     number,
   });
 
-  console.log(result);
+  const fromBranch = CIRCLE_BRANCH;
+  const toBranch = ref;
 
-  // const fromBranch = CIRCLE_BRANCH;
-  // const toBranch = result.base.ref;
-  //
-  // console.log(`PR: ${fromBranch} => ${toBranch}`);
+  console.log(`PR: ${fromBranch} => ${toBranch}`);
+
+  switch (true) {
+    case fromBranch === 'master' && toBranch === 'master':
+    case fromBranch === 'staging' && toBranch === 'staging':
+    case fromBranch === 'staging' && toBranch === 'master':
+    case fromBranch.startsWith('dev/') && toBranch === 'staging':
+    case fromBranch.startsWith('ops/') && toBranch === 'master':
+      break;
+    default:
+      throw new Error('Invalid branch rules.');
+  }
 
   process.exit(0);
 })().catch(err => {
