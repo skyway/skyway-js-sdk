e76b8166563e256dabe709d3993b1ac56128b1e7
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-03-18 12:57:41 +0900

    Wip ensure-branch

--- scripts/ensure-branch.js
+++ scripts/ensure-branch.js
@@ -1,9 +1,27 @@
 const Octokit = require('@octokit/rest');
+const { CIRCLE_BRANCH, CIRCLE_PULL_REQUEST, GITHUB_TOKEN } = process.env;
 
 (async function() {
-  const octokit = new Octokit();
-  console.log(process.env);
-  console.log(octokit);
+  if (!CIRCLE_BRANCH) {
+    throw new Error(
+      'The build not associated with a pull request is not allowed!'
+    );
+  }
+
+  // eg. https://github.com/skyway/skyway-js-sdk/pull/155
+  const [, , , owner, repo, , number] = CIRCLE_PULL_REQUEST.split('/');
+
+  const octokit = new Octokit({ auth: GITHUB_TOKEN });
+  const result = await octokit.pulls.get({
+    owner,
+    repo,
+    number,
+  });
+
+  const fromBranch = CIRCLE_BRANCH;
+  const toBranch = result.base.ref;
+
+  console.log(`PR: ${fromBranch} => ${toBranch}`);
 
   process.exit(0);
 })().catch(err => {
