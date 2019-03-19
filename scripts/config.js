fd6c469e71dab53c1ea9f70d1dc7a98b474b519b
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-03-19 09:16:08 +0900

    Add master release

--- scripts/config.js
+++ scripts/config.js
@@ -1,10 +1,17 @@
 module.exports = {
   staging: {
-    SERVER_DOMAIN: 'stage.gcp.skyway.io',
     API_KEY: '32466e1c-c9fc-4986-a0da-ba0fb96fcdc6',
+    SERVER_DOMAIN: 'stage.gcp.skyway.io',
     CDN_DOMAIN: 'cdn.stage.gcp.skyway.io',
     S3_SDK_BUCKET: 'eclrtc-cdn-gcp-staging',
     S3_EXAMPLES_BUCKET: 'eclrtc-example-gcp-staging',
   },
-  production: {},
+  master: {
+    API_KEY: '5bea388b-3f95-4e1e-acb5-a34efdd0c480',
+    // do not need to replace(= default is master)
+    SERVER_DOMAIN: '',
+    CDN_DOMAIN: '',
+    S3_SDK_BUCKET: 'eclrtc-cdn-production',
+    S3_EXAMPLES_BUCKET: 'eclrtc-example-production',
+  },
 };
---
+++ scripts/release-master.js
@@ -0,0 +1,26 @@
+const replaceExamplesApiKey = require('./shared/replace-examples-api-key');
+const uploadSdkToS3 = require('./shared/uploadSdkToS3');
+const uploadExamplesToS3 = require('./shared/uploadExamplesToS3');
+
+(async function() {
+  const {
+    master: { API_KEY, S3_SDK_BUCKET, S3_EXAMPLES_BUCKET },
+  } = require('./config');
+
+  console.log('# Replace API key for examples');
+  await replaceExamplesApiKey(API_KEY);
+  console.log('');
+
+  console.log('# Upload SDK to S3:master');
+  await uploadSdkToS3(S3_SDK_BUCKET);
+  console.log('');
+
+  console.log('# Upload examples to S3:master');
+  await uploadExamplesToS3(S3_EXAMPLES_BUCKET);
+  console.log('');
+
+  process.exit(0);
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
